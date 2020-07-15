d739b6713fe1a4b592bc613f3d5d52955683ad2b
Juan Vignolo <juavig@gmail.com>
2020-07-15 10:24:19 -0300

    Create Clase-7.py

---
+++ curso_antel/Clase-7.py
@@ -0,0 +1,49 @@
+'''
+Ejercicio 1
+Escribir una función que reciba una lista de tuplas, 
+y que devuelva un diccionario en donde las claves sean los primeros elementos de las tuplas, 
+y los valores una lista con los segundos.
+
+Por ejemplo:
+lista = [ (’Hola’, ’don Pepito’), (’Hola’, ’don Jose’), (’Buenos’, ’días’) ]
+print (tuplas_a_diccionario(lista))
+Deberá mostrar: { ’Hola’: [’don Pepito’, ’don Jose’], ’Buenos’: [’días’] }
+
+-----------------------------------------
+'''
+def diccionario(lista):
+    diccionario = {}
+    for dato in lista:
+        if dato[0] not in diccionario:
+            diccionario.update({dato[0]:[dato[1]]})
+        elif dato[0] in diccionario:
+            diccionario[dato[0]].append(dato[1])
+
+    print(diccionario)
+
+lista = [("Hola","Don Pepito"),("Hola","Don José"),("Buenos", "Días")]
+diccionario(lista)
+
+'''
+Ejercicio 2
+Escribir una función para imprimir un diccionario donde las claves sean los números 
+entre 1 y n, n es un número natural recibido como parámetro (ambos incluidos) y los valores son 
+los cuadrados de la clave
+
+Ejemplo:
+{1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81, 10: 100, 11: 121, 12: 144, 
+13: 169, 14: 196, 15: 225}
+'''
+
+'''
+Ejercicio 3
+
+a) Escribir una función que reciba una cadena y devuelva un diccionario con la cantidad de apariciones 
+de cada palabra en la cadena.
+Por ejemplo, si recibe "Qué lindo día que hace hoy"debe devolver: ’que’: 2, ’lindo’: 1, ’día’: 1, 
+’hace’: 1, ’hoy’: 1
+
+b) Escribir una función que cuente la cantidad de apariciones de cada caracter en una cadena de texto, 
+y los devuelva en un diccionario.
+
+'''
