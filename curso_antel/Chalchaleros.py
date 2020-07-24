dae3ef771629f7b0aa71093d600521bd22eea000
pelos <juavig@gmail.com>
2020-07-23 22:35:55 -0300

    Update Chalchaleros.py

--- curso_antel/Chalchaleros.py
+++ curso_antel/Chalchaleros.py
@@ -1,7 +1,6 @@
 import random
 
 def baldeacorazones(numero):
-    #numero = int(input("Ingrese la gente a chequear:"))
     artista = ["Chalchalero", "Rolling Stone"]
     integrantes = [random.choice(artista) for n in range(0, numero)]
     contador_de_rollingas = 0
@@ -12,5 +11,9 @@ def baldeacorazones(numero):
         if persona == "Chalchalero":
             contador_de_chalchaleros = contador_de_chalchaleros + 1
     print("En una lista de {} personas, hay {} Rolling Stone y {} Chalchaleros.".format(len(integrantes),contador_de_rollingas,contador_de_chalchaleros))
+    if contador_de_chalchaleros > len(integrantes) / 2:
+        print("Se baldearon menos de la mitad de corazones")
+    else:
+        print("Mas de la mitad de corazones Baldeados")
 
 baldeacorazones(int(input("Ingresee la cantidad de gente a chequear:")))
