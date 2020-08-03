a2afdf3530d4e9617b067b7ca0276646c29104b4
pelos <juavig@gmail.com>
2020-08-03 16:29:48 -0300

    Create Clase-12.py

---
+++ curso_antel/Clase-12.py
@@ -0,0 +1,26 @@
+# class Test(Exception):
+#     # constructor de la clase
+#     def __init__(self, v, m):
+#         self.valor = v
+#         self.mensaje = m
+#
+#     # la clase Test tiene 2 atributos, valor y mensaje
+#     # cada vez que creo un objeto de esta clase le
+#     # debo pasar datos para esos 2 atributos, que se #almacenan gracias al constructor
+#
+#     def __str__(self):
+#         return self.valor + ", " + self.mensaje
+#
+#
+# o1 = Test(56, "pepe")
+# o2 = Test(9898, "Holaquetal")
+#
+# print(o1.valor)  # 56
+# print(o2.mensaje)  # "Holaquetal"
+# print(o1)
+
+#Ejercicio
+'''
+INTERPRETAR EL STRING COMO OPERADOR, USAR EVAL
+TENERMOS QUE FORZAR ERRORES
+'''
