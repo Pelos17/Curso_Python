aed4842b6a0b2a071e183c7c6b3c7bc3fd68a13e
pelos <juavig@gmail.com>
2020-08-04 10:56:24 -0300

    Update Clase-12.py

--- curso_antel/Clase-12.py
+++ curso_antel/Clase-12.py
@@ -20,7 +20,34 @@
 # print(o1)
 
 #Ejercicio
+'''Escribir una función que reciba dos números (ingresados por el 
+usuario) y a partir de las operaciones especificadas en un archivo 
+(cuyo nombre deberá ser provisto como parámetro), realice los 
+cálculos y devuelva el resultado final de cada línea (resultado que 
+se obtiene operando los dos números recibidos). El archivo 
+contendrá por línea una operación aritmética sencilla; por ej:
++
+/
+**
+Nota: investigar la función de Python llamada eval.
+En caso de producirse un error durante la lectura de una línea del 
+archivo, se debe lanzar una excepción propia que informe número 
+de línea, operación problemática y motivo del error. '''
+
 '''
 INTERPRETAR EL STRING COMO OPERADOR, USAR EVAL
 TENERMOS QUE FORZAR ERRORES
 '''
+
+def operador(n1,n2):
+    with open('./curso_antel/operadores','r') as archivo:
+        archivo = archivo.read().splitlines()
+        print("Se ingresaron los numeros {} y {}.".format(n1,n2))
+        print("El resultado de las operacioens es:")
+        for x in archivo:
+            cadena = str(n1) + x + str(n2)
+            print("Operacion:",x)
+            print(eval(cadena))
+
+
+operador(420,69)
