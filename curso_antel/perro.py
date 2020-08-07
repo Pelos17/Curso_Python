02462c9a9f467e75cb8bc625c3ba542ab1ca25d7
pelos <juavig@gmail.com>
2020-08-07 10:31:55 -0300

    Create perro.py

---
+++ curso_antel/perro.py
@@ -0,0 +1,28 @@
+class Perro():
+    tipo = "Mamifero"
+    def __init__(self,nombre,edad):
+        self.nombre = nombre
+        self.edad = edad
+    def __str__(self):
+        return "El perro {} tiene {} años.".format(self.nombre,self.edad)
+    def ladrido(self,ladrido):
+        return ('El perro {} ladra {}'.format(self.nombre,ladrido))
+    def __gt__(self,otro_perro):
+        return self.edad > otro_perro.edad
+    def __ge__(self,otro_perro):
+        return self.edad >= otro_perro.edad
+    def __len__(self):
+        return self.edad
+
+
+perro1 = Perro ("Tobi",20)
+perro2 = Perro ("Puchi",11)
+perro3 = Perro ("Lalo",3)
+
+lista_perros = sorted([perro1,perro2,perro3])
+#Para sacar el mas viejo 
+print("El perro mas viejo es: {}".format(lista_perros[-1].nombre))
+#Ladrido
+print(perro1.ladrido("Bark, bark"))
+#Len
+print(len(perro1))
