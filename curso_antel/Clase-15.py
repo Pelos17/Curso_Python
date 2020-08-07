24f23828c3163e25dff3e05f3c0f055872133f77
pelos <juavig@gmail.com>
2020-08-07 10:31:59 -0300

    Update Clase-15.py

--- curso_antel/Clase-15.py
+++ curso_antel/Clase-15.py
@@ -1,3 +1,4 @@
+#Archivo perro
 class Perro():
     tipo = "Mamifero"
     def __init__(self,nombre,edad):
@@ -20,22 +21,24 @@ perro2 = Perro ("Puchi",11)
 perro3 = Perro ("Lalo",3)
 
 lista_perros = sorted([perro1,perro2,perro3])
-#Para sacar el mas viejo 
+#Para sacar el mas viejo
 print("El perro mas viejo es: {}".format(lista_perros[-1].nombre))
 #Ladrido
 print(perro1.ladrido("Bark, bark"))
 #Len
 print(len(perro1))
 
+#Archivo salchicha
 
-'''Crear una clase que ereda de perro, se llama salchica y tiene el atrivuto altura'''
+from perro import Perro
 
 class Salchicha(Perro):
-    def __init__(self,altura):
-        self.altura = altura
-    def __str__(self):
-        return "El salchica {} tiene {} años y mide {} cm.".format(self.nombre,self.edad,self.altura)
+    # def __init__(self,altura):
+    #     self.altura = altura
+    # def __str__(self):
+    #     return "El salchica {} tiene {} años y mide {} cm.".format(self.nombre,self.edad,self.altura)
+    pass
 
-salchicha1 = Salchicha("Pepe",20,30)
+salchicha1 = Salchicha("Pepe",20)
 
 print(str(salchicha1))
