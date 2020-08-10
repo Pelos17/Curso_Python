b86426a4bb89356fd38bdb4d47001d3cc6ed5c22
Juan Vignolo <juavig@gmail.com>
2020-08-10 10:44:00 -0300

    Create Clase-16.py

---
+++ curso_antel/Clase-16.py
@@ -0,0 +1,29 @@
+
+#Ejercicio 1
+
+# class Numbers():
+#     multiplier = 7
+#     def __init__(self,x,y):
+#         if int(x) and int(y):
+#             self.x = x
+#             self.y = y
+#         else:
+#             raise TypeError
+#     def multiplicar(self):
+#         return self.x*self.y*self.multiplier
+#     @property
+#     def tupla(self):
+#         return (self.x,self.y)
+
+# cuenta1 = Numbers(3,6)
+# cuenta2 = Numbers(6,9)
+# cuenta3 = Numbers("a",3)
+
+# print(cuenta1.multiplicar())
+# print(cuenta1.tupla)
+# print(cuenta2.multiplicar())
+# print(cuenta2.tupla)
+# print(cuenta3.multiplicar())
+# print(cuenta3.tupla)
+
+#Ejercicio 2
