62eb7d1e4a15435c36b3004427085eadc11f8b14
pelos <juavig@gmail.com>
2020-08-15 20:44:31 -0300

    Delete nombres_populares.py

--- curso_antel/nombres_populares.py
+++
@@ -1,8 +0,0 @@
-def extraer_nombre(archivo):
-    with open(archivo,"r") as archivo:
-        archivo = archivo.read()
-        print(archivo)
-
-
-
-extraer_nombre("./archivos-nombres/baby1990.html")
