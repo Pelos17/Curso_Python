adc73b985a002f9e6197e70b68cb9a8d4666168b
pelos <juavig@gmail.com>
2020-08-15 20:42:30 -0300

    Update Clase-12.py

--- curso_antel/Clase-12.py
+++ curso_antel/Clase-12.py
@@ -1,81 +1,92 @@
-# class Test(Exception):
-#     # constructor de la clase
-#     def __init__(self, v, m):
-#         self.valor = v
-#         self.mensaje = m
+#Ejercicio 1
+# def ejercicio1():
+#     archivo_usuario = input("Ingrese la ubicacion del archivo:")
+#     numero = int(input("Ingrese el numero:"))
+#     with open (archivo_usuario,"r") as archivo:
+#         archivo = archivo.readlines()
+#         if numero > len(archivo):
+#             for x in archivo:
+#                 print(x)
+#         else:
+#             for x in archivo[0:numero]:
+#                 print(x)
+# Profe
+# def lectura_primeros_n(archivo,n):
+#     with open(archivo,"r") as archivo:
+#         for x in range(n):
+#             print(archivo.readline())
+# ejercicio1()
 
-#     # la clase Test tiene 2 atributos, valor y mensaje
-#     # cada vez que creo un objeto de esta clase le
-#     # debo pasar datos para esos 2 atributos, que se #almacenan gracias al constructor
+#Ejercicio 2
 
-#     def __str__(self):
-#         return str(self.valor) + ", " + self.mensaje
+# def ejercicio2():
+#     archivo_usuario = input("Ingrese la ubicacion del archivo:")
+#     copia = archivo_usuario + "_copia"
+#     with open (archivo_usuario,"r") as archivo:
+#         archivo = archivo.read()
+#         print("Archivo original")
+#         print(archivo)
+#         print("*"*40)
+        
+#     with open(copia,"w") as archivo2:
+#         archivo2.write(archivo)
+#     with open(copia,"r") as archivo3:
+#         print("copia del archivo")
+#         print(archivo3.read())
 
 
-# o1 = Test(56, "pepe")
-# o2 = Test(9898, "Holaquetal")
+# ejercicio2()
 
-# print(o1.valor)  # 56
-# print(o2.mensaje)  # "Holaquetal"
-# print(o1)
+#Ejercicio 3
 
-#Ejercicio
-'''Escribir una función que reciba dos números (ingresados por el 
-usuario) y a partir de las operaciones especificadas en un archivo 
-(cuyo nombre deberá ser provisto como parámetro), realice los 
-cálculos y devuelva el resultado final de cada línea (resultado que 
-se obtiene operando los dos números recibidos). El archivo 
-contendrá por línea una operación aritmética sencilla; por ej:
-+
-/
-**
-Nota: investigar la función de Python llamada eval.
-En caso de producirse un error durante la lectura de una línea del 
-archivo, se debe lanzar una excepción propia que informe número 
-de línea, operación problemática y motivo del error. '''
+# import datetime
+#
+# def ejercicio3(archivo,texto):
+#     with open(archivo,"a") as archivo:
+#         archivo.write("\n")
+#         archivo.write(texto)
+#         archivo.write(" ")
+#         archivo.write(str(datetime.datetime.now()))
+#
+#Profe
 
+# def guarda_en_log(archivo,texto):
+#     with open(archivo,"a") as log:
+#         #log.writelines([texto,' ',str(datetime.datetime.now()),"\n"])
+#         log.write(f'{texto} {datetime.datetime.now()} \n')
+
+# ejercicio3("archivo","Prueba 3")
+
+#Ejercicio 4
+
+'''Usando la biblioteca glob, leer de un directorio todos los archivos llamados notas_dddd.txt, donde dddd son 4 dígitos que representan un año.
+El formato de estos archivos es:
+nombre_alumno; nota
+nombre_alumno; nota
+
+....
+
+nombre_alumno; nota
+Se deben imprimir los promedios por año,  y el promedio general de notas.
 '''
-INTERPRETAR EL STRING COMO OPERADOR, USAR EVAL
-TENERMOS QUE FORZAR ERRORES
-'''
-# class ErrorOperador(Exception):
-#     def __init__ (self,valor,mensaje):
-#         self.mensaje = mensaje
-#         self.valor = valor
-#     def __str__(self):
-#         return str("Linea {}.Error:{}".format(self.valor,self.mensaje))
 
-# Con este edito SyntaxError
-# def operador(n1,n2):
-#     with open('./curso_antel/operadores','r') as archivo:
-#         archivo = archivo.read().splitlines()
-#         contador = 0
-#         print("Se ingresaron los numeros {} y {}.".format(n1,n2))
-#         print("El resultado de las operacioens es:")
-#         for x in archivo:
-#             cadena = str(n1) + x + str(n2)
-#             print("Operacion:",x)
-#             contador = contador + 1
-#             try:
-#                 print(eval(cadena))
-#             except SyntaxError as e:
-#                 print('Se ingreso un operador incompatible y se genero un error {} debido a la operacion {} ingresada en la linea {} del archivo de operadores.'.format(type(e),x,contador))
-
-#Uso mi error
-# def operador(n1,n2):
-#     with open('./curso_antel/operadores','r') as archivo:
-#         archivo = archivo.read().splitlines()
-#         contador = 0
-#         print("Se ingresaron los numeros {} y {}.".format(n1,n2))
-#         print("El resultado de las operacioens es:")
-#         for x in archivo:
-#             cadena = str(n1) + x + str(n2)
-#             print("Operacion:",x)
-#             contador = contador + 1
-#             if x not in ["/","*","-","+"]:
-#                 raise ErrorOperador(contador,"Operador incorrecto")
-#             else:
-#                 print(eval(cadena))
+# import glob
+#
+# archivos = glob.glob('./notas/notas_[0-9][0-9][0-9][0-9]')
+# total = []
+# for x in archivos:
+#     año = str(x)
+#     with open(x,"r") as x:
+#         alumnos = x.readlines()
+#         suma = 0
+#         for y in alumnos:
+#             y = int(y[-2:])
+#             suma = suma+y
+#             total.append(y)
+#         print('Promedio año {}:{}'.format(año[-4:],suma/len(alumnos)))
+# suma_total = 0
+# for n in total:
+#     suma_total = suma_total + n
+# print("El promedio total es:",suma_total/len(total))
 
 
-# operador(420,69)
