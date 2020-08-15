bad8c951fb20688ab18cc80054847e45c10866be
pelos <juavig@gmail.com>
2020-08-15 20:28:37 -0300

    Update ejercicio-requests.py

--- curso_antel/ejercicio-requests.py
+++ curso_antel/ejercicio-requests.py
@@ -5,13 +5,11 @@ url_base = "http://www.montevideo.gub.uy/ubicacionesRest/calles/?nombre="
 def busca_calles():
     parametro = input("Ingrese string a buscar:")
     url_buscar= url_base+parametro
-    r = requests.get(url_buscar,headers={"User-Agent":"Juan"})
-    print(r.text)
-    if r.text == None:
-        print("No se encontro una poronga")
+    r = requests.get(url_buscar,headers={"User-Agent":"Manuel"})
+    datos=json.loads(r.text)
+    for x in datos:
+        print(x["nombre"])
+    if datos == []:
+        print("No se encontro el string.")
 
 busca_calles()
-
-# payload = {'key1': 'value1', 'key2': 'value2'}
-# r = requests.get("https://httpbin.org/get", data=payload)
-# print(r.text)
